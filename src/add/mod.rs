70dd8609521d649a444fe4da83b20e313139b14a
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-03-06 11:31:45 +0100

    feat: add file reading functionality to 'add' command and integrate Blob creation

--- src/add/mod.rs
+++ src/add/mod.rs
@@ -1,3 +1,18 @@
+use std::{fs, str::FromStr};
+
+use blob::Blob;
+
 pub fn add_to_local_repo(arg: String) {
     println!("{}", arg);
+    let read_file = fs::read(arg);
+    let file: Vec<u8>;
+    match read_file {
+        Ok(file_as_string) => file = file_as_string,
+        Err(e) => {
+            lrncore::logs::error_log(&format!("Failed to read the file: {}", e));
+            return;
+        }
+    }
+    let new_blob = Blob::from(file);
+    println!("{}", new_blob);
 }
