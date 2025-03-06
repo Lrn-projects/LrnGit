2d6df4e099c1b32c22fb4c9bfb306cee5280cffc
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-03-06 12:22:18 +0100

    feat: update blob dependency to 0.3.0 and implement SHA-1 hashing for file content in 'add' command

--- src/add/mod.rs
+++ src/add/mod.rs
@@ -1,11 +1,13 @@
-use std::{fs, str::FromStr};
+use core::hash;
+use sha1::{Digest, Sha1, Sha1Core};
+use std::{fmt::format, fs};
 
-use blob::Blob;
+use blob::{Blob, Standard};
 
 pub fn add_to_local_repo(arg: String) {
     println!("{}", arg);
-    let read_file = fs::read(arg);
-    let file: Vec<u8>;
+    let read_file = fs::read_to_string(arg);
+    let file: String;
     match read_file {
         Ok(file_as_string) => file = file_as_string,
         Err(e) => {
@@ -13,6 +15,10 @@ pub fn add_to_local_repo(arg: String) {
             return;
         }
     }
-    let new_blob = Blob::from(file);
-    println!("{}", new_blob);
+    let mut new_hash = Sha1::new();
+    new_hash.update(file);
+    let hash_result = new_hash.finalize();
+    let new_blob: Blob<Standard> = Blob::from(hash_result.to_vec());
+    let hash_result_hex = format!("{:#x}", hash_result);
+    println!("{}", hash_result_hex);
 }
