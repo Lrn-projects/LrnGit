d4ade502d72f05c10ce6d4ea838763c261656c90
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-03-07 14:39:26 +0100

    feat: enhance tree structure with TreeEntry and update recursive_add to handle SHA-1 hashes

--- src/add/mod.rs
+++ src/add/mod.rs
@@ -1,4 +1,5 @@
 use sha1::{Digest, Sha1};
+use std::fmt::LowerHex;
 use std::fs::{self, File};
 use std::io::Write;
 
@@ -6,30 +7,72 @@ use blob::{Blob, Standard};
 
 use crate::utils;
 
+/// The `TreeEntry` struct in Rust represents an entry in a tree object with mode, name, and SHA-1 hash.
+///
+/// Properties:
+///
+/// * `mode`: The `mode` property in the `TreeEntry` struct represents the file mode or permissions of
+/// the entry. It is typically a 32-bit unsigned integer that specifies the file type and permissions,
+/// such as whether the entry is a file, directory, or symbolic link, and the read, write,
+/// example: if the mode is `40000` it's a folder, else if it's `100644` it's a blob,
+/// 160000 would be a commit
+/// * `name`: The `name` property in the `TreeEntry` struct represents the name of the entry in the
+/// tree. It is of type `String` and stores the name of the entry.
+/// * `sha1`: The `sha1` property in the `TreeEntry` struct is an array of 20 unsigned 8-bit integers
+/// (bytes). This array is used to store the SHA-1 hash value of the file or directory represented by
+/// the `TreeEntry`. The SHA-1 hash is typically used to
+struct TreeEntry {
+    mode: u32,
+    name: String,
+    sha1: [u8; 20],
+}
+
+struct Tree {
+    entries: Vec<TreeEntry>,
+}
+
 pub fn add_to_local_repo(arg: String) {
     let mut folder_vec: Vec<&str> = Vec::new();
     if arg.contains("/") {
         let folder_split: Vec<&str> = arg.split("/").collect();
         folder_vec = folder_split;
     }
-    recursive_add(folder_vec, "".to_string());
+    recursive_add(folder_vec, [0u8; 20], "".to_string());
 }
 
-fn add_tree(folder: &str) {
+fn add_tree(folder: &str, child: [u8; 20], name: &str) {
     let mut new_hash = Sha1::new();
     new_hash.update(folder);
     let hash_result = new_hash.finalize();
     let folder_hash = format!("{:#x}", hash_result);
+    let new_tree_entry: TreeEntry = TreeEntry {
+        mode: (),
+        name: name.to_string(),
+        sha1: child,
+    };
 }
 
-fn add_blob(arg: &str) -> String {
+/// The function `add_blob` reads a file, calculates its SHA-1 hash, creates a new blob, and stores the
+/// file in a local repository with error handling.
+///
+/// Arguments:
+///
+/// * `arg`: The function `add_blob` takes a reference to a string `arg` as a parameter. This function
+/// reads the contents of a file specified by the `arg`, calculates its SHA-1 hash, creates a new blob
+/// from the hash, and then stores the blob in a local repository.
+///
+/// Returns:
+///
+/// The function `add_blob` returns a `String` which is the hexadecimal representation of the SHA-1 hash
+/// of the file content that was read and added to the local repository.
+fn add_blob(arg: &str) -> [u8; 20] {
     let read_file = fs::read_to_string(arg);
     let file: String;
     match read_file {
         Ok(file_as_string) => file = file_as_string,
         Err(e) => {
             lrncore::logs::error_log(&format!("Failed to read the file: {}", e));
-            return "".to_string();
+            return [0u8; 20];
         }
     }
     let mut new_hash = Sha1::new();
@@ -56,24 +99,36 @@ fn add_blob(arg: &str) -> String {
                 "Failed to add file to local repository",
                 &e.to_string(),
             );
-            return "".to_string();
+            return [0u8; 20];
         }
     }
     file_result.write_all(&new_blob).unwrap();
-    hash_result_hex
+    hash_result.into()
 }
 
-fn recursive_add(mut arg_vec: Vec<&str>, child: String) {
+/// The `recursive_add` function in Rust recursively processes elements in a vector and performs
+/// different actions based on whether the last element contains a period or not.
+///
+/// Arguments:
+///
+/// * `arg_vec`: arg_vec is a vector of string references that contains the elements being processed
+/// recursively in the function.
+/// * `child`: The `child` parameter in the `recursive_add` function seems to represent a string value
+/// that is either empty or contains some data. It is used as an argument in the function calls to
+/// `add_tree` and `recursive_add`.
+fn recursive_add(mut arg_vec: Vec<&str>, mut child: [u8; 20], mut name: String) {
+    if arg_vec.is_empty() {
+        return;
+    }
     let last = arg_vec.last().unwrap();
-    // println!("last {}", last);
-    let mut child: String = "".to_string();
     if last.contains(".") {
         let file_path = utils::concat_elem_vec(arg_vec.clone());
         let new_blob = add_blob(&file_path);
-        child = new_blob
+        child = new_blob;
+        name = last.to_string();
     } else {
-        let new_tree = add_tree(&last);
+        add_tree(&last, child.clone(), &name);
     }
     arg_vec.pop();
-    recursive_add(arg_vec, child);
+    recursive_add(arg_vec, child, name);
 }
