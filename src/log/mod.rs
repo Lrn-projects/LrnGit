df78c5a31d799682c3a5acd86dae97fdcc995aac
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-04-23 10:52:26 +0200

    feat: add unwind commits recursive function and make logs work

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -1,6 +1,10 @@
 use std::{env, io::Read, process::exit};
 
-use crate::{branch, commit, utils};
+use crate::{
+    branch,
+    commit::{self, CommitContent, InitCommitContent},
+    utils,
+};
 
 pub fn log_command() {
     let args: Vec<String> = env::args().collect();
@@ -19,17 +23,48 @@ pub fn log_command() {
     }
 }
 
+/// log all commits
 fn log_commits() {
     let last_commit = branch::parse_current_branch();
-    let mut commit_object = utils::get_file_by_hash(&last_commit);
+    let last_commit_buf = last_commit.as_bytes().to_vec();
+    let mut commits_vec: (Vec<CommitContent>, InitCommitContent) = (
+        vec![],
+        InitCommitContent {
+            tree: [0u8; 20],
+            author: vec![],
+            commiter: vec![],
+            message: vec![],
+        },
+    );
+    commits_vec = unwind_commits(last_commit_buf, commits_vec);
+    println!("all commits {:?}", commits_vec);
+}
+
+fn unwind_commits(
+    commit_hash: Vec<u8>,
+    mut commits: (Vec<CommitContent>, InitCommitContent),
+) -> (Vec<CommitContent>, InitCommitContent) {
+    let mut commit_object = utils::get_file_by_hash(
+        str::from_utf8(&commit_hash).expect("Failed to convert buffer to str"),
+    );
     let mut content_buf: Vec<u8> = Vec::new();
     commit_object
         .read_to_end(&mut content_buf)
         .expect("Failed to read commit content");
+    // decode buffer using zlib
     let mut d = flate2::read::ZlibDecoder::new(content_buf.as_slice());
     let mut buffer = Vec::new();
+    // read decoded file and populate buffer
     d.read_to_end(&mut buffer).unwrap();
 
-    let commit = commit::parse_commit(buffer);
-    println!("debug: {:?}", String::from_utf8_lossy(&commit.unwrap().parent));
+    let parse_commit = commit::parse_commit(buffer.clone());
+    let init_commit: InitCommitContent;
+    if parse_commit.is_err() {
+        init_commit = commit::parse_init_commit(buffer).unwrap();
+        commits.1 = init_commit;
+        return commits;
+    };
+    let commit_unwrapped = parse_commit.unwrap();
+    commits.0.push(commit_unwrapped.clone());
+    unwind_commits(commit_unwrapped.parent.to_vec(), commits)
 }
