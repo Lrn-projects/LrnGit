c6b59d8b2b0d16e69392b06699bbd57915f5c26f
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-17 17:36:53 +0200

    fix(server): read packet from stream

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::TcpStream;
 
 pub fn handle_client(mut stream: TcpStream) {
@@ -6,10 +6,17 @@ pub fn handle_client(mut stream: TcpStream) {
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => break, // Connection was closed
-            Ok(n) => {
-                print!("Packet received: {:?}", &buffer[..n]);
+            Ok(_n) => {
+                let stream_reader = BufReader::new(&stream);
+                let packet: Vec<_> = stream_reader
+                    .lines()
+                    .map(|result| result.unwrap())
+                    .take_while(|line| !line.is_empty())
+                    .collect();
+
+                println!("Packet: {packet:#?}");
                 // Echo everything received back to the client
-                if let Err(e) = stream.write_all(&buffer[..n]) {
+                if let Err(e) = stream.write_all("Packet received".as_bytes()) {
                     eprintln!("Failed to send response: {e}");
                     break;
                 }
