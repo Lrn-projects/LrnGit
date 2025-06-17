73791f84f65f1d1bb5c3ab5e936b1f3bb4533443
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-17 17:44:52 +0200

    fix(server): print packet from string as str

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,4 +1,4 @@
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{Read, Write};
 use std::net::TcpStream;
 
 pub fn handle_client(mut stream: TcpStream) {
@@ -6,15 +6,9 @@ pub fn handle_client(mut stream: TcpStream) {
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => break, // Connection was closed
-            Ok(_n) => {
-                let stream_reader = BufReader::new(&stream);
-                let packet: Vec<_> = stream_reader
-                    .lines()
-                    .map(|result| result.unwrap())
-                    .take_while(|line| !line.is_empty())
-                    .collect();
-
-                println!("Packet: {packet:#?}");
+            Ok(n) => {
+                let received = str::from_utf8(&buffer[..n]).unwrap();
+                println!("Packet: {received:#?}");
                 // Echo everything received back to the client
                 if let Err(e) = stream.write_all("Packet received".as_bytes()) {
                     eprintln!("Failed to send response: {e}");
