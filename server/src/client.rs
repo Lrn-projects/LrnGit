7ecb42f35318b17fce78c917fbfa5f69129729b7
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-17 17:07:46 +0200

    feat: add simple tcp server and update config.toml for linux compilation

---
+++ server/src/client.rs
@@ -0,0 +1,22 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+pub fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(0) => break, // Connection was closed
+            Ok(n) => {
+                // Echo everything received back to the client
+                if let Err(e) = stream.write_all(&buffer[..n]) {
+                    eprintln!("Failed to send response: {e}");
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to read from socket: {e}");
+                break;
+            }
+        }
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+mod client;
+
+fn main() -> std::io::Result<()> {
+    // Listening on port 7878
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    println!("Server listening on port 7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                std::thread::spawn(move || {
+                client::handle_client(stream);
+                });
+            }
+            Err(e) => {
+                eprintln!("Connection failed: {e}");
+            }
+        }
+    }
+    Ok(())
 }
