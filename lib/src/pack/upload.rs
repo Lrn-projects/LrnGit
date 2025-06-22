51490159410a56825224b13e1c4c5ba973e7be13
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-22 22:42:43 +0200

    feat(lib): add parse upload pack function with just debug

--- lib/src/pack/upload.rs
+++ lib/src/pack/upload.rs
@@ -1,18 +1,31 @@
-use serde::Serialize;
+use std::process::exit;
 
-#[derive(Serialize)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UploadPack {
     pub header: Box<[u8]>,
     pub data: Vec<UploadPackData>,
     pub footer: Box<[u8]>,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UploadPackData {
     pub header: Box<[u8]>,
     pub object_type: Box<[u8]>,
     pub hash: [u8; 20],
-    pub data: Box<[u8]>
+    pub data: Box<[u8]>,
 }
 
-
+/// Parse a slice of bytes and return an upload-pack
+pub fn parse_upload_pack(pack_slice: &[u8]) {
+    let parsed_pack: UploadPack = match bincode::deserialize(pack_slice) {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("Error parsing tree");
+            exit(1)
+            // return Err(Box::new(e));
+        }
+    };
+    println!("debug pack: {parsed_pack:?}");
+}
