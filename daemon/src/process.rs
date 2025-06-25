71d3f99bfe96d0ff5b8aa03426e3df679d49c373
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-25 14:55:32 +0200

    feat(service): try to fix the write in stdin

--- daemon/src/process.rs
+++ daemon/src/process.rs
@@ -1,14 +1,23 @@
-use std::{io::Write, process::{Command, Stdio}};
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
 
 pub fn spawn_service(name: &str, arg: &str) {
     let mut process = Command::new(name)
         .stdin(Stdio::piped())
         .spawn()
         .expect("Failed to execute asked lrngit-service");
-    let mut stdin = process.stdin.take().expect("Failed to open asked service stdin");
-    stdin.write_all(arg.as_bytes()).expect("Failed to write in asked service stdin"); 
-    let wait_process = process.wait_with_output().expect("Failed to wait asked service");
-    if !wait_process.stderr.is_empty() {
-        println!("Error in the service asked ?");
+    let mut stdin = process
+        .stdin
+        .take()
+        .expect("Failed to open asked service stdin");
+    stdin
+        .write_all(arg.as_bytes())
+        .expect("Failed to write in asked service stdin");
+    drop(stdin);
+    let wait = process.wait().expect("Failed to wait the process");
+    if !wait.success() {
+        panic!("PANIC")
     }
 }
