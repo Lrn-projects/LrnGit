3f718d47c70cc2fc1c1bd8530cf9065f045daf6a
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-26 20:59:48 +0200

    feat(service): add flush on stderr on all error

--- services/receive/src/main.rs
+++ services/receive/src/main.rs
@@ -1,21 +1,24 @@
 use std::{
     env::{self, set_current_dir},
-    io::{self, stdout, Read, Write},
+    io::{self, Read, Write, stdout},
     path::Path,
     process::exit,
 };
 
 fn main() {
     println!("[SERVICE] lrngit-receive");
+    // let stream = unsafe { TcpStream::from_raw_fd(0) };
     let args: Vec<String> = env::args().collect();
     let lrngit_repo_path: &str = "/home/ubuntu/lrngit/repositories/";
     if args.len() < 2 {
         eprintln!("ERR: repository name argument missing");
+        io::stderr().flush().unwrap();
         exit(1);
     }
     let repo_path = lrngit_repo_path.to_owned() + &args[1];
     if !Path::new(&repo_path).exists() {
         eprintln!("ERR repository doesn't exist");
+        io::stderr().flush().unwrap();
         exit(1)
     }
     set_current_dir(repo_path).expect("Failed to change current dir");
