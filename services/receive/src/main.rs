9b25aa8888b7dd3a5065b18df74c429dc00f56a8
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-26 15:55:16 +0200

    debug(service): try to fix the for loop

--- services/receive/src/main.rs
+++ services/receive/src/main.rs
@@ -9,20 +9,23 @@ fn main() {
     println!("[SERVICE] lrngit-receive");
     let args: Vec<String> = env::args().collect();
     let lrngit_repo_path: &str = "/home/ubuntu/lrngit/repositories/";
-    let repo_path = lrngit_repo_path.to_owned() + &args[0];
+    if args.len() < 2 {
+        eprintln!("ERR: repository name argument missing");
+        exit(1);
+    }
+    let repo_path = lrngit_repo_path.to_owned() + &args[1];
     if !Path::new(&repo_path).exists() {
         eprintln!("ERR repository doesn't exist");
         exit(1)
     }
     set_current_dir(repo_path).expect("Failed to change current dir");
-    let mut stdin = io::stdin();
     let mut buffer = [0u8; 1024];
     loop {
-        let n = stdin.read(&mut buffer).expect("read failed");
+        let n = io::stdin().read(&mut buffer).expect("read failed");
 
         if n == 0 {
-            println!("TCP connection closed");
-            stdout().flush().unwrap();
+            eprintln!("TCP connection closed");
+            io::stderr().flush().unwrap();
             break;
         }
 
