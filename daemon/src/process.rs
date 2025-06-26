c49d83426dfafc7e6df5024294c50d1f5ebe8b20
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-26 20:35:04 +0200

    feat(daemon): try to inherite fd to forked process

--- daemon/src/process.rs
+++ daemon/src/process.rs
@@ -1,14 +1,27 @@
 use std::{
     net::TcpStream,
-    os::fd::{FromRawFd, IntoRawFd},
+    os::fd::{FromRawFd, IntoRawFd, RawFd},
     process::{Command, Stdio},
 };
+
+use nix::fcntl::{self, FcntlArg};
+
+fn make_fd_inheritable(fd: RawFd) {
+    use std::os::fd::BorrowedFd;
+    // SAFETY: fd must be valid and open for the duration of this function
+    let borrowed_fd = unsafe { BorrowedFd::borrow_raw(fd) };
+    let flags = fcntl::fcntl(borrowed_fd, fcntl::FcntlArg::F_GETFD).unwrap();
+    let mut flags = fcntl::FdFlag::from_bits_truncate(flags);
+    flags.remove(fcntl::FdFlag::FD_CLOEXEC);
+    fcntl::fcntl(borrowed_fd, fcntl::FcntlArg::F_SETFD(flags)).unwrap();
+}
 
 pub fn fork_service(name: &str, arg: &str, socket: TcpStream) {
     let fd = {
         let fd = socket.into_raw_fd();
         fd
     }; // socket drop
+    make_fd_inheritable(fd);
     let process = Command::new(name)
         .stdin(unsafe { Stdio::from_raw_fd(fd) })
         .stdout(unsafe { Stdio::from_raw_fd(fd) })
