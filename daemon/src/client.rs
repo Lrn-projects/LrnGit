198f590af31c92aeae08d440087335fe40f4f27d
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-26 10:07:46 +0200

    feat(service & daemon): try to fix core dump and dropstream problem

--- daemon/src/client.rs
+++ daemon/src/client.rs
@@ -5,47 +5,47 @@ use crate::process;
 
 pub fn handle_client(mut stream: TcpStream) {
     let mut buffer: [u8; 512] = [0; 512];
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(0) => break, // Connection was closed
-            Ok(n) => {
-                let received = String::from_utf8_lossy(&buffer[..n]);
-                println!("Packet: {received:#?}");
-                let mut service: Vec<u8> = Vec::new();
-                let mut path: Vec<u8> = Vec::new();
-                let mut temp: bool = false;
-                for &b in buffer.iter() {
-                    if b == b' ' {
-                        temp = true;
-                    }
-                    if !temp {
-                        service.push(b);
-                    } else {
-                        if b == b'\0' {
-                            break;
-                        }
-                        path.push(b);
-                    }
+    match stream.read(&mut buffer) {
+        Ok(0) => drop(stream), // Connection was closed
+        Ok(n) => {
+            let received = String::from_utf8_lossy(&buffer[..n]);
+            println!("Packet: {received:#?}");
+            let mut service: Vec<u8> = Vec::new();
+            let mut path: Vec<u8> = Vec::new();
+            let mut temp: bool = false;
+            for &b in buffer.iter() {
+                if b == b' ' {
+                    temp = true;
                 }
-                let service_str: &str = str::from_utf8(&service).unwrap();
-                let path_str: &str = str::from_utf8(&path).unwrap().trim();
-                match service_str {
-                    "lrngit-receive-pack" => {
-                        process::fork_service("lrngit-receive-service", path_str, &stream);
+                if !temp {
+                    service.push(b);
+                } else {
+                    if b == b'\0' {
+                        break;
                     }
-                    "lrngit-upload-pack" => {}
-                    _ => {}
-                }
-                // Send response to client when packet received
-                if let Err(e) = stream.write_all("Packet received".as_bytes()) {
-                    eprintln!("Failed to send response: {e}");
-                    break;
+                    path.push(b);
                 }
             }
-            Err(e) => {
-                eprintln!("Failed to read from socket: {e}");
-                break;
+            let service_str: &str = str::from_utf8(&service).unwrap();
+            let path_str: &str = str::from_utf8(&path).unwrap().trim();
+            match service_str {
+                "lrngit-receive-pack" => {
+                    process::fork_service("lrngit-receive-service", path_str, &stream);
+                    drop(stream);
+                    return;
+                }
+                "lrngit-upload-pack" => {}
+                _ => {}
             }
+            // Send response to client when packet received
+            if let Err(e) = stream.write_all("Packet received".as_bytes()) {
+                eprintln!("Failed to send response: {e}");
+                drop(stream);
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to read from socket: {e}");
+            drop(stream);
         }
     }
 }
--- services/receive/src/main.rs
+++ services/receive/src/main.rs
@@ -9,13 +9,15 @@ fn main() {
     println!("[SERVICE] lrngit-receive");
     let args: Vec<String> = env::args().collect();
     let lrngit_repo_path: &str = "/home/ubuntu/lrngit/repositories/";
-    let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input).unwrap();
-    println!("debug stdin: {input:?}");
     let repo_path = lrngit_repo_path.to_owned() + &args[1];
     if !Path::new(&repo_path).exists() {
         eprintln!("ERR repository doesn't exist");
         exit(1)
     }
     set_current_dir(repo_path).expect("Failed to change current dir");
+    let mut input = String::new();
+    loop {
+        std::io::stdin().read_to_string(&mut input).unwrap();
+        println!("debug stdin: {input:?}");
+    }
 }
