c26f413df156962b132a1c41a7b0313ce3abaf37
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-26 21:08:24 +0200

    feat(service): try to fix the print in stdio

--- services/receive/src/main.rs
+++ services/receive/src/main.rs
@@ -1,11 +1,12 @@
 use std::{
-    env::{self, set_current_dir}, io::{self, stdout, Read, Write}, net::TcpStream, os::fd::FromRawFd, path::Path, process::exit
+    env::{self, set_current_dir},
+    io::{self, Read, Write},
+    path::Path,
+    process::exit,
 };
 
 fn main() {
     println!("[SERVICE] lrngit-receive");
-    let mut stream = unsafe { TcpStream::from_raw_fd(0) };
-    stream.write_all("prout".as_bytes()).unwrap();
     let args: Vec<String> = env::args().collect();
     let lrngit_repo_path: &str = "/home/ubuntu/lrngit/repositories/";
     if args.len() < 2 {
@@ -23,7 +24,6 @@ fn main() {
     let mut buffer = [0u8; 1024];
     loop {
         let n = io::stdin().read(&mut buffer).expect("read failed");
-
         if n == 0 {
             eprintln!("TCP connection closed");
             io::stderr().flush().unwrap();
@@ -31,6 +31,6 @@ fn main() {
         }
 
         println!("Packet: {:?}", String::from_utf8_lossy(&buffer[..n]));
-        stdout().flush().unwrap();
+        io::stdout().flush().unwrap();
     }
 }
