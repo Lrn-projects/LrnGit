7f5c402eb8ea233a3b43aedd35f4b37e2b72939c
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-26 23:29:22 +0200

    debug(service): try to fix stderr and args

--- services/receive/src/main.rs
+++ services/receive/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    env::{self, set_current_dir}, io::{self, Read, Write}, net::TcpStream, os::fd::FromRawFd, path::Path, process::exit
+    env::{self, set_current_dir}, io::{self, Read, Write}, path::Path, process::exit
 };
 
 fn main() {
@@ -8,22 +8,20 @@ fn main() {
     let lrngit_repo_path: &str = "/home/ubuntu/lrngit/repositories/";
     if args.len() < 2 {
         eprintln!("ERR: repository name argument missing");
-        io::stderr().flush().unwrap();
         exit(1);
     }
     let repo_path = lrngit_repo_path.to_owned() + &args[0];
     if !Path::new(&repo_path).exists() {
         eprintln!("ERR repository doesn't exist");
-        io::stderr().flush().unwrap();
         exit(1)
     }
+    println!("debug: {:?}", repo_path);
     set_current_dir(repo_path).expect("Failed to change current dir");
     let mut buffer = [0u8; 1024];
     loop {
         let n = io::stdin().read(&mut buffer).expect("read failed");
         if n == 0 {
             eprintln!("TCP connection closed");
-            io::stderr().flush().unwrap();
             break;
         }
 
