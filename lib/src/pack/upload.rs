5e53513891d1ac216b62fb2a304e14880790ba00
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-07-01 12:31:17 +0200

    debug(service): try fix on parsing upload pack

--- lib/src/pack/upload.rs
+++ lib/src/pack/upload.rs
@@ -3,9 +3,16 @@ use std::io::{Error, ErrorKind};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type", content = "data")]
+pub enum Data {
+    Bytes(Vec<Vec<u8>>),
+    Structs(Vec<UploadPackData>),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UploadPack {
     pub header: Box<[u8]>,
-    pub data: Vec<UploadPackData>,
+    pub data: Data,
     pub footer: Box<[u8]>,
 }
 
@@ -18,13 +25,29 @@ pub struct UploadPackData {
 }
 
 /// Parse a slice of bytes and return an upload-pack
-pub fn parse_upload_pack(pack_slice: &[u8]) -> Result<UploadPack, Error>{
-    let parsed_pack: UploadPack = match bincode::deserialize(pack_slice) {
+pub fn parse_upload_pack(pack_slice: &[u8]) -> Result<UploadPack, Error> {
+    let mut parsed_pack: UploadPack = match bincode::deserialize(pack_slice) {
         Ok(p) => p,
         Err(e) => {
             eprintln!("Error parsing upload pack: {e:?}");
-            return Err(Error::new(ErrorKind::Other, e))
+            return Err(Error::new(ErrorKind::Other, e));
         }
     };
+    let mut data_vec: Vec<UploadPackData> = Vec::new();
+    match &parsed_pack.data {
+        Data::Bytes(vec) => {
+            for each in vec {
+                match bincode::deserialize::<UploadPackData>(each) {
+                    Ok(o) => data_vec.push(o),
+                    Err(e) => {
+                        eprintln!("Error parsing upload pack object: {e:?}");
+                        return Err(Error::new(ErrorKind::Other, e));
+                    }
+                }
+            }
+        }
+        Data::Structs(_upload_pack_datas) => {}
+    }
+    parsed_pack.data = Data::Structs(data_vec);
     Ok(parsed_pack)
 }
