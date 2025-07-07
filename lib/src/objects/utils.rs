45ad5682c40835cd55f1dededcd2bf850bf2ec47
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-07-07 12:01:55 +0200

    feat(lib): add get_all_object function

--- lib/src/objects/utils.rs
+++ lib/src/objects/utils.rs
@@ -1,3 +1,5 @@
+use std::{fs::read_dir, path::PathBuf, str::FromStr};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -15,4 +17,15 @@ pub fn split_hash(hash: &str) -> String {
     path
 }
 
-
+pub fn get_all_object(path: &str, object_vec: &mut Vec<PathBuf>) {
+    let object_path = PathBuf::from_str(path).expect("Failed to create new pathbuf from object path");
+    for object_read_dir in read_dir(object_path).unwrap() {
+        let object = object_read_dir.expect("Failed to get object in objects");
+        let object_metadata = object.metadata().expect("Failed to get object metadata");
+        if object_metadata.is_file() {
+            object_vec.push(object.path());
+        } else if object_metadata.is_dir() {
+            get_all_object(object.path().as_os_str().to_str().expect("Failed to cast os_str to str"), object_vec);
+        }
+    }
+}
