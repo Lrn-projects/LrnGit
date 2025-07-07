0857c1b1af0e70f160db744e8941578449182d41
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-07-07 22:32:45 +0200

    feat(client): update parsing tree entries to use the tree creation fix buffer

--- client/src/parser.rs
+++ client/src/parser.rs
@@ -1,20 +1,19 @@
 use std::{error::Error, io::Read};
 
-use lrngitcore::objects::tree::{Tree, TreeEntry};
-
+use lrngitcore::objects::{tree::{Tree, TreeEntry}, utils::split_object_header};
 
 pub fn parse_tree_entries_obj(buff: Vec<u8>) -> Result<Vec<TreeEntry>, Box<dyn Error>> {
     let mut d = flate2::read::ZlibDecoder::new(buff.as_slice());
     let mut buffer = Vec::new();
     d.read_to_end(&mut buffer)
         .expect("Failed to read tree buffer for parsing");
-
-    let entries: Tree = match bincode::deserialize(&buffer) {
+    let (_, content) = split_object_header(buffer);
+    let entries: Vec<TreeEntry> = match bincode::deserialize(&content) {
         Ok(c) => c,
         Err(e) => {
             eprintln!("Error parsing tree");
             return Err(Box::new(e));
         }
     };
-    Ok(entries.entries)
+    Ok(entries)
 }
