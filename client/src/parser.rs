afbe5c3c1cef1be7e749bb0723506a6e154191cb
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-07-07 22:35:41 +0200

    chore(client): remove unused import

--- client/src/parser.rs
+++ client/src/parser.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, io::Read};
 
-use lrngitcore::objects::{tree::{Tree, TreeEntry}, utils::split_object_header};
+use lrngitcore::objects::{tree::TreeEntry, utils::split_object_header};
 
 pub fn parse_tree_entries_obj(buff: Vec<u8>) -> Result<Vec<TreeEntry>, Box<dyn Error>> {
     let mut d = flate2::read::ZlibDecoder::new(buff.as_slice());
