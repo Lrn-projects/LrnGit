048e54b62f7b36b1da3cfb0649c4832977a98381
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-07-21 20:31:27 +0200

    fix(lib): use u32 instead of usize

--- lib/src/out.rs
+++ lib/src/out.rs
@@ -7,7 +7,7 @@ use std::io::Write;
 /// msg: message to write in stdout.
 /// stdout: ptr to impl of Write.
 pub fn write_framed_message_stdout(msg: &str, stdout: &mut impl Write) {
-    let length: usize = msg.len();
+    let length: u32 = msg.len().try_into().expect("Failed to cast usize to u32");
     let length_slice = length.to_le_bytes();
     stdout.write_all(&length_slice).expect("Failed to write length to stdout");
     stdout.write_all(&msg.as_bytes()).expect("Failed to write message to stdout");
